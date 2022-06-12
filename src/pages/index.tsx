82fa2c07e31c4a3046f07361724f5513fd718200
Arthur Sena <senaarth@gmail.com>
2022-06-12 15:44:29 -0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Head from "next/head";
+
+export default function Home(): JSX.Element {
+  return (
+    <div>
+      <Head>
+        <title>Home | Project Name</title>
+      </Head>
+    </div>
+  );
+}
+
